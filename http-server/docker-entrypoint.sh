a61e9ae48a8036c03d3794ae812d5ba35367649d
luthean03 <lucas.pontes@epfedu.fr>
2025-05-22 08:55:29 +0200

    test

--- http-server/docker-entrypoint.sh
+++ http-server/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Remplace ${CONTAINER_PROXY} dans le template
-envsubst '$CONTAINER_PROXY' \
+envsubst '$CONTAINER_BACKEND' \
   < /usr/local/apache2/conf/httpd.conf.template \
   > /usr/local/apache2/conf/httpd.conf
 
